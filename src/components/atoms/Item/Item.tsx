4a29481475af5d8a0cac98dc45f3a69b44a3672d
Anderson Lozada <andersonlozada@flare.com.co>
2024-09-22 13:24:54 -0500

    build: :dizzy: animations with aos

--- src/components/atoms/Item/Item.tsx
+++ src/components/atoms/Item/Item.tsx
@@ -4,7 +4,7 @@ import { ItemProps } from './ItemProps.types';
 
 const ItemComponent: React.FC<ItemProps> = ({ item }) => {
   return (
-    <div className='home__list__card'>
+    <div className='home__list__card' data-aos="fade-up">
       <h3 className='home__card__title'>{item.title}</h3>
       <p>{item.body}</p>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ItemList } from '../components/molecules/ItemList';
 import { useItemContext } from '../context/ItemContext';
 import { Item } from '../interfaces/Item.interface';
 import { ItemForm } from '../components/organism/ItemForm';
 import { Typography } from '../components/atoms/Typography'; 
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home: React.FC = () => {
   const { items, addItem } = useItemContext();
@@ -18,10 +19,13 @@ const Home: React.FC = () => {
       console.error("Error adding item:", error); 
     }
   };
+    useEffect(() => {
+      AOS.init({ duration: 1000, once: true });
+    }, []);
 
   return (
     <div className='home'>
-      <div className='home__container'>
+      <div className='home__container' data-aos="fade-up">
         <Typography 
           text="Add New Item" 
           tag="h1" 
@@ -36,7 +40,7 @@ const Home: React.FC = () => {
           tag="h2" 
           className="home__list__title" 
         />
-        <ItemList items={currentItem ? [currentItem] : items} /> 
+        <ItemList items={currentItem ? [currentItem] : items}  /> 
       </div>
     </div>
   );
